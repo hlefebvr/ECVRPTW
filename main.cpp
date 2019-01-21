dd4d9018cb3421ba85bf6b1cd477780b2b914af3
Henri Lefebvre <henri.pasdecalais@yahoo.fr>
2019-01-21 13:10:49 +0100

    fix two instances, station location

--- main.cpp
+++ main.cpp
@@ -12,14 +12,14 @@ int main() {
             "../input/Instance1_2s_30c_4v.txt",
             "../input/Instance2_2s_30c_4v.txt",
             "../input/Instance2_2s_40c_4v.txt",
-            "../input/Instance3_2s_40c_4v.txt",
+            "../input/Instance3_2s_40c_4v.txt", //
             "../input/Instance4_2s_40c_4v.txt",
             "../input/Instance4_3s_45c_6v.txt"
     };
     const string output = "./output.csv";
-    const double max_execution_time = 10;
+    const double max_execution_time = 60;
 
-    Instance::load_from_file(inputs[0]); // ok for 0, 1, 2 ko for 3, 4, 5
+    Instance::load_from_file(inputs[3]); // ok for 0, 1, 2 ko for 3, 4, 5
 
     auto solver = Solver(max_execution_time);
 
